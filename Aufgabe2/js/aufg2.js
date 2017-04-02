8fc90e2f4e546dd86f4e6bb38ec34c15094beb85
ThinkPad User <ThinkPad User@X220>
2017-04-02 11:47:01 +0200

    Aufgabe 2 erstellt

--- Aufgabe2/js/aufg2.js
+++ Aufgabe2/js/aufg2.js
@@ -1,16 +1,26 @@
+/*
+Aufgabe: 2
+Name: Jana Burger
+Matrikel: 255076
+Datum: 16.03.17
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
+Er wurde nicht kopiert und auch nicht diktiert.
+
+Die Aufgabe wurde in Zusammenarbeit mit Selina Mazzaro und Jacqueline Wagner erstellt.
+*/
 document.addEventListener('DOMContentLoaded', function () {
-    var blackColor = "#000000";
-    var whiteColor = "#ffffff";
-    var n = 64;
-    var x = 1;
-    var zeile = 8;
-    for (var i = 0; i < n; i++) {
-        Div();
-    }
-    ;
+    var n = 64; //felder
+    var x = 0; //gerade zahl: wei�, ungerade zahl: schwarz
+    var a = 1; //Reis
+    var i = 0;
+    var zeile = 0;
+    // Divs
     function Div() {
         var div = document.createElement("div");
         document.body.appendChild(div);
+        div.className = "box";
+        //Style der Divs
         var s = div.style;
         s.borderStyle = "solid";
         s.borderColor = "#000000";
@@ -18,6 +28,43 @@ document.addEventListener('DOMContentLoaded', function () {
         s.width = "50px";
         s.height = "50px";
         s.display = "inline-block";
+        s.overflow = "auto";
+        div.innerText = "" + a;
+        a = a * 2;
+    }
+    for (i; i < n; i++) {
+        Div();
+        if (x % 2 == zeile % 2) {
+            whiteColor();
+        }
+        else {
+            blackColor();
+        }
+        if (x % 8 == 0) {
+            zeile++;
+        }
+    }
+    //wei�e Felder
+    function whiteColor() {
+        var div = document.getElementsByTagName("div");
+        var farbe = div[x].getElementsByClassName("box");
+        if (div.length != 0) {
+            div[x].style.backgroundColor = "white";
+            div[x].style.color = "black";
+            div[x].style.textAlign = "center";
+            x++;
+        }
+    }
+    //schwarze Felder
+    function blackColor() {
+        var div = document.getElementsByTagName("div");
+        var farbe = div[x].getElementsByClassName("box");
+        if (div.length != 0) {
+            div[x].style.backgroundColor = "black";
+            div[x].style.color = "white";
+            div[x].style.textAlign = "center";
+            x++;
+        }
     }
 });
 //# sourceMappingURL=aufg2.js.map
--- Aufgabe2/js/aufg2.ts
+++ Aufgabe2/js/aufg2.ts
@@ -1,24 +1,78 @@
-document.addEventListener('DOMContentLoaded', function() {
+/*
+Aufgabe: 2 
+Name: Jana Burger
+Matrikel: 255076
+Datum: 16.03.17
     
-    let blackColor: string = "#000000";
-    let whiteColor: string = "#ffffff";
-    let n: number = 64;
-    let x: number = 1;
-    let zeile: number = 8;
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
+Er wurde nicht kopiert und auch nicht diktiert.
 
-for (let i: number = 0; i < n; i++) {
-    Div();       
-};
-function Div(): void {
-    let div: HTMLDivElement = document.createElement("div");
-    document.body.appendChild(div);
+Die Aufgabe wurde in Zusammenarbeit mit Selina Mazzaro und Jacqueline Wagner erstellt.
+*/
 
-    let s: CSSStyleDeclaration = div.style;
-    s.borderStyle = "solid";
-    s.borderColor = "#000000";
-    s.borderWidth = "1px";
-    s.width = "50px";
-    s.height = "50px";
-    s.display = "inline-block";
-}    
-    });
+document.addEventListener('DOMContentLoaded', function() {
+
+    let n: number = 64; //felder
+    let x: number = 0; //gerade zahl: weiß, ungerade zahl: schwarz
+    let a: number = 1; //Reis
+    let i: number = 0;
+    let zeile: number = 0;
+
+
+    // Divs
+    function Div(): void {
+        let div: HTMLDivElement = document.createElement("div");
+        document.body.appendChild(div);
+        div.className = "box";
+        
+    //Style der Divs
+        let s: CSSStyleDeclaration = div.style;
+        s.borderStyle = "solid";
+        s.borderColor = "#000000";
+        s.borderWidth = "1px";
+        s.width = "50px";
+        s.height = "50px";
+        s.display = "inline-block";
+        s.overflow = "auto";
+        div.innerText = "" + a;
+        a = a * 2;
+    }
+    
+    for (i; i < n; i++) {
+        Div();
+        if (x % 2 == zeile % 2) {
+            whiteColor();
+        }
+        else {
+            blackColor();
+        }
+        if (x % 8 == 0 ) {
+            zeile++;
+        }
+    }
+    
+   //weiße Felder
+    function whiteColor(): void {
+        let div = document.getElementsByTagName ("div");
+        let farbe = div[x].getElementsByClassName("box");
+        if (div.length != 0) {
+            div[x].style.backgroundColor = "white";
+            div[x].style.color = "black";
+            div[x].style.textAlign = "center";
+            x++;    
+        }
+    }
+    
+    //schwarze Felder
+    function blackColor(): void {
+        let div = document.getElementsByTagName ("div");
+        let farbe = div[x].getElementsByClassName ("box");
+        if (div.length != 0) {
+            div[x].style.backgroundColor = "black";
+            div[x].style.color = "white";
+            div[x].style.textAlign = "center";
+            x++;
+        }
+    }
+     
+});
