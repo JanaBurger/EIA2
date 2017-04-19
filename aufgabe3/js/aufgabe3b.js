b72d03ae13d574953385bc0f5dcf1235729838bc
ThinkPad User <ThinkPad User@X220.hfu-open.fu.hs-furtwangen.de>
2017-04-19 12:39:08 +0200

    3b überarbeitet

--- aufgabe3/js/aufgabe3b.js
+++ aufgabe3/js/aufgabe3b.js
@@ -6,45 +6,47 @@ Datum: 9.4.17
     
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe.
 Er wurde nicht kopiert und auch nicht diktiert.*/
-/*Diese Aufgabe wurde in Zusammenarbeit mit Jacqueline und Selina erstellt
-wir kamen an einem Punkt leider nicht mehr weiter und hatten keine Idee mehr wie wir unsere Fehler l�sen k�nnen.
-Es w�re gut wenn wir das im Praktikum nochmal ausf�hrlich besprechen k�nnten.*/
+var cards;
+var handcards = [];
+var ablagecards = [];
+var nachziehstapel;
+var ablagestapel;
 document.addEventListener("DOMContentLoaded", function () {
-    var cards = [
+    cards = [
         "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz Koenig", "Herz Ass",
         "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik Koenig", "Pik Ass",
         "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo Koenig", "Karo Ass",
         "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz Koenig", "Kreuz Ass"
     ];
-    var handcards;
-    var ablagecards;
-    var nachziehstapel = document.getElementById("nachziehstapel");
-    var ablagestapel = document.getElementById("ablagestapel");
-    nachziehstapel.addEventListener("click", ClickEvent);
-    function ClickEvent() {
-        //if (typeof handcards !== "undefined") {  <-- wenn ich das drin lasse, verschwindet die Fehlermeldung, jedoch 
-        //                                             werden keine divs mehr erzeugt :(
-        //if (cards.length > 0 && handcards.length < 5) { <-- leider funktioniert die Funktion nicht mehr sobald ich die if 
-        //                                                    Anweisung aktiviere
-        var div = document.createElement("div"); //create div
-        document.getElementById("handstapel").appendChild(div); //anh�ngen an handkarten
-        var wert = Math.floor((Math.random() * 31) + 0);
-        div.className = "selected";
-        div.textContent = cards[wert];
+    nachziehstapel = document.getElementById("nachziehstapel");
+    ablagestapel = document.getElementById("ablagestapel");
+    nachziehstapel.addEventListener("click", drawCard);
+    ablagestapel.addEventListener("click", removeCard);
+});
+function drawCard() {
+    //if (typeof handcards !== "undefined") {  <-- wenn ich das drin lasse, verschwindet die Fehlermeldung, jedoch 
+    //                                             werden keine divs mehr erzeugt :(
+    if (cards.length > 0 && handcards.length < 5) {
+        var div_1 = document.createElement("div"); //create div
+        document.getElementById("handstapel").appendChild(div_1); //anh�ngen an handkarten
+        var wert = Math.floor((Math.random() * (cards.length - 1)) + 0);
+        div_1.className = "selected";
+        div_1.textContent = cards[wert];
         var auswahl = cards[wert];
         cards.splice(wert, 1);
         handcards.push(auswahl);
-        div.addEventListener("click", function () {
-            for (var i = 0; i < handcards.length; i++) {
-                if (this.textContent == handcards[wert]) {
-                    ablagecards.push(handcards[i]);
-                    handcards.splice(i, 1);
-                    break;
-                }
-                this.parentNode.removeChild(this);
-            }
-        });
-        console.log(cards);
+        div_1.addEventListener("click", removeCard);
     }
-});
+}
+function removeCard(_event) {
+    var target = _event.target;
+    console.log(target);
+    for (var i = 0; i < handcards.length; i++) {
+        if (handcards[i] === target.textContent) {
+            ablagecards.push(handcards.splice(i, 1)[0]);
+            target.parentNode.removeChild(target);
+        }
+    }
+    ablagestapel.textContent = ablagecards[ablagecards.length - 1];
+}
 //# sourceMappingURL=aufgabe3b.js.map
--- aufgabe3/js/aufgabe3b.ts
+++ aufgabe3/js/aufgabe3b.ts
@@ -7,61 +7,62 @@ Datum: 9.4.17
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. 
 Er wurde nicht kopiert und auch nicht diktiert.*/
 
-/*Diese Aufgabe wurde in Zusammenarbeit mit Jacqueline und Selina erstellt
-wir kamen an einem Punkt leider nicht mehr weiter und hatten keine Idee mehr wie wir unsere Fehler lösen können.
-Es wäre gut wenn wir das im Praktikum nochmal ausführlich besprechen könnten.*/
+
+let cards: string[];
+let handcards: string[] = [];
+let ablagecards: string[] = [];
+let nachziehstapel: HTMLElement;
+let ablagestapel: HTMLElement;
+
 
 document.addEventListener("DOMContentLoaded", function(): void {
-    
-    let cards: string[] = [
-    "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz Koenig", "Herz Ass",
-    "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik Koenig", "Pik Ass",
-    "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo Koenig", "Karo Ass",
-    "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz Koenig", "Kreuz Ass"
-    ];
-    
-    let handcards: string[];
-    let ablagecards: string[]; 
-    let nachziehstapel: HTMLElement = document.getElementById("nachziehstapel");
-    let ablagestapel: HTMLElement = document.getElementById("ablagestapel");
 
-    
-    
-    nachziehstapel.addEventListener("click", ClickEvent); 
-  
-         
-    function ClickEvent(): void {
-        //if (typeof handcards !== "undefined") {  <-- wenn ich das drin lasse, verschwindet die Fehlermeldung, jedoch 
-        //                                             werden keine divs mehr erzeugt :(
-        //if (cards.length > 0 && handcards.length < 5) { <-- leider funktioniert die Funktion nicht mehr sobald ich die if 
-        //                                                    Anweisung aktiviere
-            
+    cards = [
+        "Herz 7", "Herz 8", "Herz 9", "Herz 10", "Herz Bube", "Herz Dame", "Herz Koenig", "Herz Ass",
+        "Pik 7", "Pik 8", "Pik 9", "Pik 10", "Pik Bube", "Pik Dame", "Pik Koenig", "Pik Ass",
+        "Karo 7", "Karo 8", "Karo 9", "Karo 10", "Karo Bube", "Karo Dame", "Karo Koenig", "Karo Ass",
+        "Kreuz 7", "Kreuz 8", "Kreuz 9", "Kreuz 10", "Kreuz Bube", "Kreuz Dame", "Kreuz Koenig", "Kreuz Ass"
+    ];
+
+    nachziehstapel = document.getElementById("nachziehstapel");
+    ablagestapel = document.getElementById("ablagestapel");
+
+    nachziehstapel.addEventListener("click", drawCard);
+    ablagestapel.addEventListener("click", removeCard);
+});
+
+function drawCard(): void {
+    //if (typeof handcards !== "undefined") {  <-- wenn ich das drin lasse, verschwindet die Fehlermeldung, jedoch 
+    //                                             werden keine divs mehr erzeugt :(
+    if (cards.length > 0 && handcards.length < 5) {
+
         let div: HTMLDivElement = document.createElement("div"); //create div
         document.getElementById("handstapel").appendChild(div); //anhängen an handkarten
-           
-            
-        let wert: number = Math.floor((Math.random() * 31) + 0);
-        
-        div.className = "selected";      
+
+
+        let wert: number = Math.floor((Math.random() * (cards.length - 1)) + 0);
+
+        div.className = "selected";
 
         div.textContent = cards[wert];
-        
+
         let auswahl: string = cards[wert];
         cards.splice(wert, 1);
-        handcards.push(auswahl); 
-      
-        div.addEventListener("click", function (): void {
-           for (let i: number = 0; i < handcards.length; i++) {
-                    if (this.textContent == handcards[wert]) {
-                   
-                        ablagecards.push(handcards[i]);
-                     
-                        handcards.splice(i, 1);
-                        break;
-        }
-        this.parentNode.removeChild(this);
-            }});
-        console.log (cards);
-  
+        handcards.push(auswahl);
+
+        div.addEventListener("click", removeCard);
     }
-});
+
+}
+
+function removeCard(_event: MouseEvent): void {
+    let target: HTMLElement = <HTMLElement>_event.target;
+    console.log(target);
+    for (let i: number = 0; i < handcards.length; i++) {
+        if (handcards[i] === target.textContent) {
+            ablagecards.push(handcards.splice(i, 1)[0]);
+            target.parentNode.removeChild(target);
+        }
+    }
+    ablagestapel.textContent = ablagecards[ablagecards.length - 1];
+}
